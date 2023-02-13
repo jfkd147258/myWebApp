315bb6747766c8f5ea37be979d8f344128b276aa
jfkd147258 <jfkd147258@gmail.com>
2023-02-13 22:14:14 +0800

    logo to white

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo bg-white" alt="logo" />
+        <img src={logo} className="App-logo white" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload!.
         </p>
